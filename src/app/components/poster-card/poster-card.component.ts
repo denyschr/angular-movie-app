f2cae5e3037b93e08aff30cb682915b1b1f115f0
denyschr <denprogman99@gmail.com>
2024-06-17 09:58:08 +0300

    feat: add primeng & update component styling

--- src/app/components/poster-card/poster-card.component.ts
+++ src/app/components/poster-card/poster-card.component.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Poster } from '../../models/poster.model';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
 import { MinutesToHoursPipe } from '../../shared/pipes/minutes-to-hours/minutes-to-hours.pipe';
 
 @Component({
   selector: 'app-poster-card',
   standalone: true,
-  imports: [CommonModule, MinutesToHoursPipe],
+  imports: [
+    CommonModule,
+    CardModule,
+    ButtonModule,
+    RatingModule,
+    MinutesToHoursPipe,
+  ],
   templateUrl: './poster-card.component.html',
   styleUrl: './poster-card.component.scss',
 })
